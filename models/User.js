1c7e5b838e5fcff9af51ccba4f3e8ac8dae402c6
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-06-08 19:38:39 -0700

    creating User model with rulesss

---
+++ models/User.js
@@ -0,0 +1,50 @@
+// creating schema model for user
+const { Schema, model } = require('mongoose');
+
+// creating schema for user
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trimmed: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// creating virtual for user
+// this is used to show the number of thoughts
+// and the number of friends
+userSchema
+  .virtual('friendCount')
+  .get(function() {
+    return this.friends.length;
+  });
+
+const User = model('user', userSchema);
+
+module.exports = User;
